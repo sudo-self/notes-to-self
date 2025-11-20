95d609dcb290f8f112dda6c286a4d2d5179004a8
sudo-self <sudo-self@proton.me>
2025-11-19 19:00:47 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Note to self",
+  title: "Notes to self",
   description: "Write notes, keep them private or share them with the team. Github Oauth and Turso DB serverless api",
 };
 
